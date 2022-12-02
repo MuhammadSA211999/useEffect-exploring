5378939f1b4d7a2b4b76c3c233098e7f2092581a
Muhammad SA <MuhammadSA211999>
2022-12-02 23:09:44 +0600

    done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import MyComponent from './componentents/MyComponent';
+
 
 function App() {
   return (
     <div className="App">
       <h1>useEffect exploring</h1>
+      <MyComponent></MyComponent>
     </div>
   );
 }
